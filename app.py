83d4b1c93ff1c51fed9b049794edb29d7ca50e7c
Anshuman776 <anshumanmaurya776@gmail.com>
2025-10-13 12:48:53 +0530

    Prepare for render

--- app.py
+++ app.py
@@ -176,7 +176,11 @@ def predict():
     )
 
 
-if __name__ == "__main__":
-    # Run without the auto-reloader to avoid interpreter shutdown issues
-    app.run(debug=False, use_reloader=False)
+# if __name__ == "__main__":
+#     # Run without the auto-reloader to avoid interpreter shutdown issues
+#     app.run(debug=False, use_reloader=False)
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
